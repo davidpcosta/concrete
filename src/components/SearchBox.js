06f097b05b146e70b5c048a28a9f105eb00cc9c4
David Costa <davidpcosta@gmail.com>
2020-02-01 23:49:06 -0300

    CSS

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -25,7 +25,7 @@ class SearchBox extends React.Component {
   render = () => {
     return (
         <form onSubmit={this.handleFormSubmit}>
-          <div>
+          <div className="searchBoxContainer">
             <input className="searchBox" type="text" value={ this.state.searchText } onChange={ this.handleTextChange } />
             <button className="searchButton" type="submit">
               <i className="ic-search"></i>
